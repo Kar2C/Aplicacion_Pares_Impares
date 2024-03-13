c226cc516149aa8498f8910bda11ac3c0f13eddc
Kar2C <125744991+Kar2C@users.noreply.github.com>
2024-03-13 00:18:55 -0500

    Se implemento CSS en la aplicación, de igual manera, se hizo la validación correspondiente para las opciones de Pares mayores e Impares mayores

--- js/index.js
+++ js/index.js
@@ -3,3 +3,21 @@ for (let i=1; i<=200; i++){
     numeros.push(i);
 }
 console.log(numeros);
+
+document.getElementById("opciones").addEventListener("change", function() { 
+    //change es un evento, se usa cuando se activa cuando se cambia de opción 
+    // function()-> es una función anonima
+    let opcionSeleccionada = this.value; //guarda el valor que tenga la opción que disparo el evento
+    let numeroContainer = document.getElementById("numero-container");
+    let verBtn = document.getElementById("ver-btn");
+
+
+    // Mostrar el contenedor de número solo cuando se selecciona la opción "Pares Mayores"
+    if (opcionSeleccionada === "pares-mayores" || opcionSeleccionada === "pares-menores") {
+        numeroContainer.style.display = "block"; //cambiando el estilo de visualización para que aparezca un bloque
+        verBtn.style.display = "inline-block"; // Mostrar el botón "Ver"
+    } else {
+        numeroContainer.style.display = "none";
+
+    }
+});
