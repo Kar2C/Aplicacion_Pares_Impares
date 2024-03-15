bab4f77fc652ff8fb00b8fe4af6f556adb9499cb
Kar2C <125744991+Kar2C@users.noreply.github.com>
2024-03-14 19:21:39 -0500

    Se agrego la posición de la impresión, animaciones, estilos css, alerta por números ingresados erroneamente

--- js/index.js
+++ js/index.js
@@ -3,12 +3,13 @@ document.getElementById('ver-btn').addEventListener('click', () => {
     const numeroContainer = document.getElementById('numero-container');
     const imprimirNum = document.getElementById('imprimir');
     let numeros = [];
-    let posicion = 0;
 
     imprimirNum.innerText = "";
 
     // Guardar el número ingresado una vez
     const numeroIngresado = parseInt(document.getElementById('numero').value);
+
+    document.getElementById('numero').value = '';
 
     if (seleccion === 'pares-mayores' || seleccion === 'impares-mayores') {
         // Mostrar el div numero-container si se selecciona una de las opciones adecuadas
@@ -17,35 +18,42 @@ document.getElementById('ver-btn').addEventListener('click', () => {
         numeroContainer.style.display = 'none';
     }
 
+    if (numeroIngresado < 0 || numeroIngresado >= 200) {
+        alert('Ingrese un número que este entre 0 y 200');
+    }
+
     for (let i = 200; i >= 1; i--) {
         switch (seleccion) {
             case 'pares':
                 if (i % 2 === 0) {
                     numeros.push(i);
-                    imprimirNum.innerText += 'Par en posición ' + (posicion++) + ': ' + i + '\n';
+                    imprimirNum.innerText += 'Par en posición ' + (i - 1) + ': ' + i + '\n';
                 }
                 break;
             case 'impares':
                 if (i % 2 === 1) {
                     numeros.push(i);
-                    imprimirNum.innerText += 'Impar en posición ' + (posicion++) + ': ' + i + '\n';
+                    imprimirNum.innerText += 'Impar en posición ' + (i - 1) + ': ' + i + '\n';
                 }
                 break;
             case 'pares-mayores':
+
                 if (i > numeroIngresado && i % 2 === 0) {
                     numeros.push(i);
-                    imprimirNum.innerText += 'Par en posición ' + (posicion++) + ': ' + i + '\n';
+                    imprimirNum.innerText += 'Par en posición ' + (i - 1) + ': ' + i + '\n';
                 }
+
                 break;
             case 'impares-mayores':
                 if (i > numeroIngresado && i % 2 === 1) {
                     numeros.push(i);
-                    imprimirNum.innerText += 'Impar en posición ' + (posicion++) + ': ' + i + '\n';
+                    imprimirNum.innerText += 'Impar en posición ' + (i - 1) + ': ' + i + '\n';
                 }
                 break;
             default:
                 break;
         }
     }
+
 
 });
